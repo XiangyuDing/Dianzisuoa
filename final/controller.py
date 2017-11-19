453577fc6e60c2aa3332f6d4f6442c840f675a36
Betsalel Williamson <saul.williamson@pitt.edu>
2017-11-19 21:16:22 +0000

    first version of final project works

--- final/controller.py
+++ final/controller.py
@@ -22,7 +22,7 @@ IN4 = 15
 
 ## record audio code
 BUFFER_SIZE = 1024
-REC_SECONDS = 5
+REC_SECONDS = 2
 RATE = 48000
 WAV_FILENAME = "temp.wav"
 FORMAT = pyaudio.paInt16
@@ -30,45 +30,52 @@ FORMAT = pyaudio.paInt16
 def record():
      pa = pyaudio.PyAudio()
      for i in range(pa.get_device_count()):
-     dev = pa.get_device_info_by_index(i)
+          dev = pa.get_device_info_by_index(i)
      print((i,dev['name'],dev['maxInputChannels']))
 
-    stream = pa.open(
-        format = FORMAT,
-        input = True,
-        channels = 1,
-        rate = RATE,
-        input_device_index = 2,
-        frames_per_buffer = BUFFER_SIZE
-    )
+     stream = pa.open(
+          format = FORMAT,
+          input = True,
+          channels = 1,
+          rate = RATE,
+          input_device_index = 2,
+          frames_per_buffer = BUFFER_SIZE
+     )
 
-    #run recording
-    print('Recording...')
-    data_frames = []
-    toRange = int(RATE/BUFFER_SIZE * REC_SECONDS)
-    for f in range(0, toRange):
-        data = stream.read(BUFFER_SIZE)
-        data_frames.append(data)
-    print('Finished recording...')
-    stream.stop_stream()
-    stream.close()
-    pa.terminate()
+     #run recording
+     print('Recording...')
+     data_frames = []
+     toRange = int(RATE/BUFFER_SIZE * REC_SECONDS)
+     for f in range(0, toRange):
+          data = stream.read(BUFFER_SIZE)
+          data_frames.append(data)
+     print('Finished recording...')
+     stream.stop_stream()
+     stream.close()
+     pa.terminate()
 
-    wf = wave.open(WAV_FILENAME, 'wb')
-    wf.setnchannels(1)
-    wf.setsampwidth(pa.get_sample_size(FORMAT))
-    wf.setframerate(RATE)
-    wf.writeframes(b''.join(data_frames))
-    wf.close()
+     wf = wave.open(WAV_FILENAME, 'wb')
+     wf.setnchannels(1)
+     wf.setsampwidth(pa.get_sample_size(FORMAT))
+     wf.setframerate(RATE)
+     wf.writeframes(b''.join(data_frames))
+     wf.close()
 
-    text = 'hello'
-    proc = subprocess.Popen(
-        ['./transcibeAudio.sh', WAV_FILENAME],stdout=subprocess.PIPE)
+     text = 'hello'
+     proc = subprocess.Popen(
+     ['./transcibeAudio.sh', WAV_FILENAME],stdout=subprocess.PIPE)
 
-    result = proc.stdout.read()
-    print result
+     result = proc.stdout.read()
+     print(result)
+     
+     with open("transcription-log.txt", "a") as myFile:
+          myFile.write(result.decode("utf-8"))
 
-    return result
+     modifiedResult = result[16:].decode("utf-8").strip()
+     print (modifiedResult)
+     # Transcription:  
+
+     return modifiedResult
 
 #step motor set up
 def setStep(w1, w2, w3, w4):
@@ -118,6 +125,7 @@ def destroy():
 # voice control 
 voice_increment=1
 keypad_determine=0
+
 voice_correct_phrase = 'open the door' # default passcode
 
 def start_recording():
--- final/transcibeAudio.sh
+++ final/transcibeAudio.sh
@@ -3,4 +3,4 @@
 # this file should be a wav file with sample rate of 48000 Hz
 # usage: ./transcribeAudio.sh <audio-file>
 
-nodejs ../google-audio-api/speech/recognize.js sync $1 -r 48000
+nodejs ../examples/google-audio-api/speech/recognize.js sync $1 -r 48000
